fcbe903a1da9b2433c0b599bd8d600bef00b0580
hoangle3 <hoang.le@vietlink.jp>
2024-07-22 18:00:46 +0700

    merge user

--- app/Http/Controllers/Auth/ProviderController.php
+++ app/Http/Controllers/Auth/ProviderController.php
@@ -17,37 +17,46 @@ class ProviderController extends Controller
     public function callback($provider)
     {
         $provided_user = Socialite::driver($provider)->user();
+
         if ($provider == 'google') {
-            $user = User::where('google_id', $provided_user->getId())->first();
+            $user = User::where('google_id', $provided_user->getID())->first();
 
             if (!$user) {
-                $new = User::create([
+                // If not found, attempt to find by email
+                $user = User::where('email', $provided_user->getEmail())->first();
+            }
+
+            if (!$user) {
+                // If still not found, create a new user
+                $user = User::create([
                     'name' => $provided_user->getName(),
                     'email' => $provided_user->getEmail(),
-                    'google_id' => $provided_user->getID()
                 ]);
-                Auth::login($new);
-                return redirect('/booking');
-            } else {
-                Auth::login($user);
-                return redirect('/booking');
             }
+            
+            $user->google_id = $provided_user->getID();
+            $user->update();
         }
         else if ($provider == 'facebook') {
             $user = User::where('facebook_id', $provided_user->getId())->first();
 
             if (!$user) {
-                $new = User::create([
+                // If not found, attempt to find by email
+                $user = User::where('email', $provided_user->getEmail())->first();
+            }
+
+            if (!$user) {
+                $user = User::create([
                     'name' => $provided_user->getName(),
                     'email' => $provided_user->getEmail(),
-                    'facebook_id' => $provided_user->getID()
                 ]);
-                Auth::login($new);
-                return redirect('/booking');
-            } else {
-                Auth::login($user);
-                return redirect('/booking');
             }
+
+            $user->facebook_id = $provided_user->getID();
+            $user->update();
         }
+        
+        Auth::login($user);
+        return redirect('/booking');
     }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
-            $table->string('google_id')->nullable();
-            $table->string('facebook_id')->nullable();
+            $table->string('google_id')->nullable()->unique();
+            $table->string('facebook_id')->nullable()->unique();
             $table->rememberToken();
             $table->timestamps();
         });
